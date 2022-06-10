735e3789ca2be4c4ec3ef7fdab4536a579c36be4
MisileLaboratory <misileminecord@gmail.com>
2022-06-11 02:43:48 +0900

    update name

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use poise::serenity_prelude as serenity;
 
 use serde::Deserialize;
 
-use misile_lib::read_user_from_file;
+use misilelib::read_user_from_file;
 
 use std::{fs::File, string::String};
 
